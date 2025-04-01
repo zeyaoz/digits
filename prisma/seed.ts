6f8cb7c28e574d7cb5171a8f2dd7506ea36bfb72
zeyaoz <zeyaoz@hawaii.edu>
2025-03-31 23:26:39 -1000

    contact forms finished in 13 minutes.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -45,6 +45,20 @@ async function main() {
       },
     });
   });
+  config.defaultContacts.forEach(async (contact, index) => {
+    await prisma.contact.upsert({
+      where: { id: index },
+      update: {},
+      create: {
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        address: contact.address,
+        image: contact.image,
+        description: contact.description,
+        owner: contact.owner,
+      },
+    });
+  });
 }
 main()
   .then(() => prisma.$disconnect())
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,10 @@
 import { getServerSession } from 'next-auth';
-import { Col, Container, Row, Table } from 'react-bootstrap';
-import StuffItemAdmin from '@/components/StuffItemAdmin';
+import { Col, Container, Row } from 'react-bootstrap';
 import { prisma } from '@/lib/prisma';
+import { Contact } from '@prisma/client';
 import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
+import ContactCardAdmin from '@/components/ContactCardAdmin';
 
 const AdminPage = async () => {
   const session = await getServerSession(authOptions);
@@ -12,52 +13,20 @@ const AdminPage = async () => {
       user: { email: string; id: string; randomKey: string };
     } | null,
   );
-  const stuff = await prisma.stuff.findMany({});
-  const users = await prisma.user.findMany({});
-
+  const contacts: Contact[] = await prisma.contact.findMany({});
   return (
     <main>
       <Container id="list" fluid className="py-3">
         <Row>
           <Col>
-            <h1>List Stuff Admin</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Quantity</th>
-                  <th>Condition</th>
-                  <th>Owner</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {stuff.map((item) => (
-                  <StuffItemAdmin key={item.id} {...item} />
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1>List Users Admin</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Email</th>
-                  <th>Role</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
+            <h1>List Contacts Admin</h1>
+            <Row xs={1} md={2} lg={3} className="g-4">
+              {contacts.map((contact) => (
+                <Col key={contact.firstName + contact.lastName}>
+                  <ContactCardAdmin contact={contact} />
+                </Col>
+              ))}
+            </Row>
           </Col>
         </Row>
       </Container>
